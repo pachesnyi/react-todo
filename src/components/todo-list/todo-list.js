0f46ef8f21b6ca997a845ab9a05746ed2a86ec05
John <ivan.pachesnyi@extrawest.com>
2020-06-09 17:36:07 +0300

    SYS: initial commit

---
+++ src/components/todo-list/todo-list.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './todo-list.css';
+
+import TodoListItem from "../todo-list-item";
+
+const TodoList = ({todos}) => {
+  const elements = todos?.map((item) => {
+    const { id, ...itemProps } = item;
+    return (
+      <li className="list-group-item" key={id}>
+        <TodoListItem {...itemProps}/>
+      </li>
+    );
+  });
+
+  return (
+    <ul className="list-group todo-list">
+      {elements}
+    </ul>
+  );
+};
+
+export default TodoList;
